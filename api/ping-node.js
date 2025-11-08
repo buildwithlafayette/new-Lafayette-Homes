5e6ecd0209de16a1af95b7a67108f72902ddd7a6
buildwithlafayette <buildwithlafayette@gmail.com>
2025-11-08 10:04:54 -0500

    ping last

--- api/ping-node.js
+++
@@ -1,6 +0,0 @@
-// /api/ping-node.js  (Node runtime)
-export default function handler(req, res) {
-  const hasKey = !!process.env.RESEND_API_KEY;
-  res.setHeader("content-type", "application/json");
-  res.status(200).send(JSON.stringify({ ok: true, runtime: "node", resend_key_present: hasKey }));
-}
